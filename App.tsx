90e658a1f28f00083b56f8cfd5a700bf6606c61f
Pranav-Ganga <pranavg2525@gmail.com>
2023-03-19 11:37:04 +0530

    Chat Screens biolerplate

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+import { NhostClient, NhostReactProvider } from "@nhost/react";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+import * as SecureStore from "expo-secure-store";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const nhost = new NhostClient({
+  subdomain: "mglohphkquqpgnlvkgen",
+  region: "ap-south-1",
+  clientStorageType: "expo-secure-storage",
+  clientStorage: SecureStore,
+});
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <GestureHandlerRootView style={{flex:1}}>
+        <SafeAreaProvider>
+        <NhostReactProvider nhost={nhost}>
+          <NhostApolloProvider nhost={nhost}>
+            <Navigation colorScheme={colorScheme} />
+          </NhostApolloProvider>
+        </NhostReactProvider>
+        <StatusBar />
+      </SafeAreaProvider>
+      </GestureHandlerRootView>
+    );
+  }
+}
